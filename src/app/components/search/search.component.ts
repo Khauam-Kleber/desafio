824964e3c68e91452c1b7496bea21ec655cd9aef
Khauam <khauam@hotmail.com>
2025-03-02 12:02:57 -0300

    Adicionando o stepper no layout apenas html e css sem função

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,15 @@
-import { AfterViewInit, Component, ElementRef, inject, signal, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, signal, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgxMaskDirective } from 'ngx-mask';
 import { UtilsService } from '../../services/utils.service';
 import { CardDetailComponent } from '../card-detail/card-detail.component';
 import { findFakeInfoByCpf, InfoCard } from '../../models/info-card.model';
 import { CommonModule } from '@angular/common';
-import { debounceTime, distinctUntilChanged, fromEvent, map } from 'rxjs';
+import { StepperComponent } from '../stepper/stepper.component';
 
 @Component({
   selector: 'app-search',
-  imports: [FormsModule, NgxMaskDirective, CardDetailComponent, CommonModule],
+  imports: [FormsModule, NgxMaskDirective, CardDetailComponent, CommonModule, StepperComponent],
   providers: [UtilsService],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -5,16 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class UtilsService {
   cpfIsValid(cpf: string): boolean {
-    // Remove caracteres não numéricos
     const cpfLimpo = cpf.replace(/\D/g, '');
-  
-    // Verifica se tem 11 dígitos
     if (cpfLimpo.length !== 11) return false;
-  
-    // Verifica se o CPF não é uma sequência repetida (ex: 111.111.111-11)
     if (/^(\d)\1{10}$/.test(cpfLimpo)) return false;
-  
-    // Função para calcular o dígito verificador
     const calcularDigito = (base: string, pesoInicial: number) => {
       let soma = 0;
       for (let i = 0; i < base.length; i++) {
@@ -23,12 +16,8 @@ export class UtilsService {
       const resto = soma % 11;
       return resto < 2 ? 0 : 11 - resto;
     };
-  
-    // Calcula os dois dígitos verificadores
     const digito1 = calcularDigito(cpfLimpo.substring(0, 9), 10);
     const digito2 = calcularDigito(cpfLimpo.substring(0, 10), 11);
-  
-    // Valida os dígitos verificadores
     return digito1 === parseInt(cpfLimpo[9]) && digito2 === parseInt(cpfLimpo[10]);
   }
 }
